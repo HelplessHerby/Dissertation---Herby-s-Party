75781f50aed288d06992422e426ac431ce619d2d
HelplessHerby <90830725+HelplessHerby@users.noreply.github.com>
2026-01-25 18:00:52 +0000

    added navmesh based movement

--- Herby's Party/Assets/Scripts/Board/BoardMovement.cs
+++ Herby's Party/Assets/Scripts/Board/BoardMovement.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BoardMovement : MonoBehaviour
 {
     public GameObject[] tileArr;
+    public NavMeshAgent agent;
     public int curTileIndex = 0;
     public IEnumerator UpdatePlayer(int moveAmount)
     {
         int ind = moveAmount + curTileIndex;
         for (int i = curTileIndex; i < ind; i++)
         {
-            transform.position = tileArr[i].transform.position;
+            agent.destination = tileArr[i].transform.position;
             yield return new WaitForSeconds(.5f);
             curTileIndex = ind;
         }
